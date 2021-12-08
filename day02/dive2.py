5e29a6617a5c9f427dca3aec8c164caebb6fa8f1
Yipten <yipten@outlook.com>
2021-12-07 21:15:12 -0500

    complete day 2 part 2

---
+++ day02/dive2.py
@@ -0,0 +1,35 @@
+# AoC 2021 Day 2 Part 2
+# Dive!
+
+# read input file into list of strings
+inputFile = open('input.txt')
+lines = inputFile.readlines()
+inputFile.close()
+# split into two lists (one for direction, one for distance)
+directions = []
+distances = []
+for line in lines:
+	lineSplit = line.split()
+	directions.append(lineSplit[0])
+	distances.append(int(lineSplit[1]))
+
+# horizontal position
+position = 0
+# depth below sea level
+depth = 0
+# aim (pitch)
+aim = 0
+
+# loop through each instruction
+for i in range(len(directions)):
+	# check which direction to move in
+	if directions[i] == 'forward':
+		position += distances[i]
+		depth += distances[i] * aim;
+	elif directions[i] == 'down':
+		aim += distances[i]
+	else:	# must be 'up'
+		aim -= distances[i]
+
+# print product of position and depth
+print(position * depth)
