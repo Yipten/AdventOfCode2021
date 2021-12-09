8c693e2ba5a7c3fcee5c514aef1eb91bc30f5a74
Yipten <yipten@outlook.com>
2021-12-09 14:19:41 -0500

    create list conversion functions

--- day04/giant_squid.py
+++ day04/giant_squid.py
@@ -1,13 +1,45 @@
 # AoC 2021 Day 4 Part 1
 # Giant Squid
 
+def convert_numbers(numbers_str):
+    '''Converts list of strings to list of integers.'''
+
+    numbers = []
+    for i in numbers_str:
+        numbers.append(int(i))
+    return numbers
+
+
+def convert_boards(boards_str):
+    '''
+    Converts 3D list of strings to list of tuples.
+
+    The first value in each tuple is the integer value of that location on the
+    bingo board. The second is a boolean representing whether that board
+    location has been marked. 
+    '''
+
+    boards = []
+    # loop through boards
+    for board in range(len(boards_str)):
+        boards.append([])
+        # loop through rows
+        for row in range(len(boards_str[board])):
+            boards[board].append([])
+            # loop through individual values
+            for i in range(len(boards_str[board][row])):
+                boards[board][row].append(
+                    (int(boards_str[board][row][i]), False))
+    return boards
+
+
 def main():
     # get input
     input_file = open('day04/input_short.txt')
     numbers_str = input_file.readline().split(',')
     # ignore blank line
     input_file.readline()
-    # array to store all board info
+    # array to store raw board info
     boards_str = []
     # used to get one board at a time from input
     temp_board = []
@@ -22,7 +54,8 @@ def main():
             # reset for next board
             temp_board = []
     input_file.close()
-    print(boards_str)
+    numbers = convert_numbers(numbers_str)
+    boards = convert_boards(boards_str)
 
 
 if __name__ == '__main__':
