bd4001f1b77f540b1c5381ece73d2096bcd00923
Yipten <yipten@outlook.com>
2021-12-11 15:54:28 -0500

    complete day 7 part 1

--- day07/treachery_of_whales.py
+++ day07/treachery_of_whales.py
@@ -5,15 +5,34 @@ def calculate_fuel_cost(crab_positions, target_position):
     '''
     Calculates the fuel cost of moving all crabs to a certain position.
     '''
-    
+
     fuel_cost = 0
     for i in crab_positions:
         fuel_cost += abs(target_position - i)
     return fuel_cost
 
+
+def calculate_min_fuel_cost(crab_positions):
+    '''
+    Calculates the minimum possible fuel cost for all crabs to move to the same
+    position.
+    '''
+
+    # find position boundaries
+    min_position = min(crab_positions)
+    max_position = max(crab_positions)
+    # find minimum fuel cost
+    min_fuel_cost = None
+    for i in range(min_position, max_position + 1):
+        fuel_cost = calculate_fuel_cost(crab_positions, i)
+        if min_fuel_cost == None or fuel_cost < min_fuel_cost:
+            min_fuel_cost = fuel_cost
+    return min_fuel_cost
+
+
 def main():
     # get input
-    input_file = open('day07/input_short.txt')
+    input_file = open('day07/input.txt')
     crab_positions_str = input_file.read()
     input_file.close()
 
@@ -22,10 +41,7 @@ def main():
     for i in crab_positions_str.split(','):
         crab_positions.append(int(i))
 
-    print(calculate_fuel_cost(crab_positions, 2))
-    print(calculate_fuel_cost(crab_positions, 1))
-    print(calculate_fuel_cost(crab_positions, 3))
-    print(calculate_fuel_cost(crab_positions, 10))
+    print(calculate_min_fuel_cost(crab_positions))
 
 
 if __name__ == '__main__':
