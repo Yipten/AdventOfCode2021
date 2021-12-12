3840e3b058017b2929c3229eb01e2440ef0c112a
Yipten <yipten@outlook.com>
2021-12-12 17:46:25 -0500

    complete day 8 part 2

--- day08/seven_segment_search_2.py
+++ day08/seven_segment_search_2.py
@@ -1,4 +1,4 @@
-# AoC Day 8 Part 1
+# AoC Day 8 Part 2
 # Seven Segment Search
 
 def identify_easy_patterns(line):
@@ -72,15 +72,27 @@ def identify_6_char_patterns(line):
     digits = list(line.values())
     pattern_of_1 = patterns[digits.index(1)]
     pattern_of_4 = patterns[digits.index(4)]
-    pattern_of_7 = patterns[digits.index(7)]
-    pattern_of_8 = patterns[digits.index(8)]
 
-    pass
+    for pattern in line.keys():
+        # skip over patterns not six characters long
+        if len(pattern) != 6:
+            continue
+        common_with_1 = get_num_common_segments(pattern, pattern_of_1)
+        common_with_4 = get_num_common_segments(pattern, pattern_of_4)
+        if common_with_1 == 1:
+            # has to be 6
+            line[pattern] = 6
+        elif common_with_4 == 3:
+            # has to be 0
+            line[pattern] = 0
+        else:   # only other option
+            # has to be 9
+            line[pattern] = 9
 
 
 def main():
     # get input
-    input_file = open('day08/input_short.txt')
+    input_file = open('day08/input.txt')
     entries = input_file.readlines()
     input_file.close()
 
@@ -92,17 +104,33 @@ def main():
         split_line = entries[i].split('|')
         # store first part of line in list of dictionaries
         pattern_lines.append({})
-        line = split_line[0].split()
-        for j in range(len(line)):
-            pattern_lines[i][line[j]] = None
+        line_part_1 = split_line[0].split()
+        for pattern in line_part_1:
+            sorted_pattern = ''.join(sorted(pattern))
+            pattern_lines[i][sorted_pattern] = None
         # store second part of line in list of lists
-        output_pattern_lines.append(split_line[1].split())
+        output_pattern_lines.append([])
+        line_part_2 = split_line[1].split()
+        for pattern in line_part_2:
+            sorted_pattern = ''.join(sorted(pattern))
+            output_pattern_lines[i].append(sorted_pattern)
 
+    # identify which digit corresponds with each pattern
     for line in pattern_lines:
         identify_easy_patterns(line)
         identify_5_char_patterns(line)
+        identify_6_char_patterns(line)
 
-    print('done')
+    # calculate the sum of the output values
+    output_sum = 0
+    for i in range(len(output_pattern_lines)):
+        str_val = ''
+        for pattern in output_pattern_lines[i]:
+            str_val += str(pattern_lines[i][pattern])
+        output_sum += int(str_val)
+
+    print('Sum of output values: ' + str(output_sum))
+
 
 if __name__ == '__main__':
     main()
