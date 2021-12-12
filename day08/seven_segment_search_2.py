cab2c7773c3e32daf7d2b54091ec3917c800d16d
Yipten <yipten@outlook.com>
2021-12-12 16:08:02 -0500

    narrow it down to possible digits first

--- day08/seven_segment_search_2.py
+++ day08/seven_segment_search_2.py
@@ -1,25 +1,41 @@
 # AoC Day 8 Part 1
 # Seven Segment Search
 
-def find_easy_digits(digit_pattern_lines):
+def identify_possible_digits(pattern):
     '''
-    Finds which patterns correspond to the digits 1, 4, 7, and 8
+    Identifies the possible digits that could be represented by each pattern
+    depending on the length of each pattern.
     '''
 
-    for line in digit_pattern_lines:
-        for i in line.keys():
-            # 2 segments used to display 1
-            if len(i) == 2:
-                line[i] = 1
-            # 4 segments used to display 4
-            elif len(i) == 4:
-                line[i] = 4
-            # 3 segments used to display 7
-            elif len(i) == 3:
-                line[i] = 7
-            # 7 segments used to display 8
-            elif len(i) == 7:
-                line[i] = 8
+    pattern_length = len(pattern)
+
+    if pattern_length == 2:
+        # 2 segments used to display 1
+        return 1
+    elif pattern_length == 3:
+        # 3 segments used to display 7
+        return 7
+    elif pattern_length == 4:
+        # 4 segments used to display 4
+        return 4
+    elif pattern_length == 5:
+        # 5 segments used to display 2, 3, and 5
+        return (2, 3, 5)
+    elif pattern_length == 6:
+        # 6 segments used to display 0, 6, and 9
+        return (0, 6, 9)
+    elif pattern_length == 7:
+        # 7 segments used to display 8
+        return 8
+
+
+def identify_digits(line):
+    '''
+    Identifies which digits correspond to each pattern.
+    '''
+
+    for pattern in line.keys():
+        line[pattern] = identify_possible_digits(pattern)
 
 
 def main():
@@ -29,20 +45,21 @@ def main():
     input_file.close()
 
     # split up input into lists
-    digit_pattern_lines = []
-    output_digit_patterns = []
+    pattern_lines = []
+    output_pattern_lines = []
     for i in range(len(entries)):
         # split line in two
         split_line = entries[i].split('|')
         # store first part of line in list of dictionaries
-        digit_pattern_lines.append({})
+        pattern_lines.append({})
         line = split_line[0].split()
         for j in range(len(line)):
-            digit_pattern_lines[i][line[j]] = None
+            pattern_lines[i][line[j]] = None
         # store second part of line in list of lists
-        output_digit_patterns.append(split_line[1].split())
+        output_pattern_lines.append(split_line[1].split())
 
-    find_easy_digits(digit_pattern_lines)
+    for line in pattern_lines:
+        identify_digits(line)
 
     print('done')
 
