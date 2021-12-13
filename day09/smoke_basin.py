a834f7198bbb1c855e2d09f0a09c265f58f6d269
Yipten <yipten@outlook.com>
2021-12-13 14:06:42 -0500

    complete day 9 part 1

--- day09/smoke_basin.py
+++ day09/smoke_basin.py
@@ -1,9 +1,56 @@
 # AoC 2021 Day 9 Part 1
 # Smoke Basin
 
+def check_vertical_neighbors(heightmap, r, c):
+    '''
+    Checks if the given point is less than both of its vertical neighbors.
+    '''
+
+    if r == 0:
+        return heightmap[r][c] < heightmap[r + 1][c]
+    elif r == len(heightmap) - 1:
+        return heightmap[r][c] < heightmap[r - 1][c]
+    return heightmap[r][c] < heightmap[r - 1][c] and heightmap[r][c] < heightmap[r + 1][c]
+
+
+def find_low_point_heights(heightmap):
+    '''
+    Find all low points and return a list of their heights.
+    '''
+
+    low_point_heights = []
+    # RC Cola
+    for r in range(len(heightmap)):
+        for c in range(len(heightmap[r])):
+            if c == 0:
+                if (heightmap[r][c] < heightmap[r][c + 1]
+                        and check_vertical_neighbors(heightmap, r, c)):
+                    low_point_heights.append(heightmap[r][c])
+            elif c == len(heightmap[r]) - 1:
+                if (heightmap[r][c] < heightmap[r][c - 1]
+                        and check_vertical_neighbors(heightmap, r, c)):
+                    low_point_heights.append(heightmap[r][c])
+            elif (heightmap[r][c] < heightmap[r][c - 1]
+                  and heightmap[r][c] < heightmap[r][c + 1]
+                  and check_vertical_neighbors(heightmap, r, c)):
+                low_point_heights.append(heightmap[r][c])
+    return low_point_heights
+
+
+def calculate_risk_level_sum(heightmap):
+    '''
+    Calculates and returns the sum of the risk levels of all low points.
+    '''
+
+    low_point_heights = find_low_point_heights(heightmap)
+    # effectively add one to each height in the list
+    risk_level_sum = sum(low_point_heights) + len(low_point_heights)
+    return risk_level_sum
+
+
 def main():
     # get input
-    input_file = open('day09/input_short.txt')
+    input_file = open('day09/input.txt')
     input_lines = input_file.readlines()
     input_file.close()
 
@@ -18,7 +65,7 @@ def main():
         for j in range(len(input_lines[i])):
             heightmap[i][j] = int(input_lines[i][j])
 
-    print('done')
+    print('Sum of risk levels: ' + str(calculate_risk_level_sum(heightmap)))
 
 
 if __name__ == '__main__':
