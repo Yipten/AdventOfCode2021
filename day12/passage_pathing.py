edd23ea312d823ca8b9d3213ba2ef9daae12e57b
Yipten <yipten@outlook.com>
2021-12-16 21:31:40 -0500

    complete day 12 part 1

---
+++ day12/passage_pathing.py
@@ -0,0 +1,49 @@
+# AoC 2021 Day 12 Part 1
+# Passage Pathing
+
+def find_num_paths(connections, current_cave='start', caves_visited=[]):
+    '''
+    Recursively finds the number of possible paths from the given cave to the
+    end cave.
+    '''
+
+    caves_visited.append(current_cave)
+    if current_cave == 'end':
+        num_paths = 1
+        # caves_visited = []  # TODO: make a replacement for this, removing one at a time moving down the stack
+    else:
+        num_paths = 0
+        for cave in connections[current_cave]:
+            if cave in caves_visited and cave.islower():
+                continue
+            num_paths += find_num_paths(connections, cave, caves_visited)
+    caves_visited.pop()
+    return num_paths
+
+
+def main():
+    # get input
+    input_file = open('day12/input.txt')
+    input_lines = input_file.readlines()
+    input_file.close()
+
+    # convert input into list of tuples representing cave connections
+    connection_pairs = []
+    for line in input_lines:
+        connection_pairs.append(tuple(line.strip().split('-')))
+
+    # convert list of tuples into dictionary
+    connections = {}
+    for pair in connection_pairs:
+        if not pair[0] in connections.keys():
+            connections[pair[0]] = []
+        connections[pair[0]].append(pair[1])
+        if not pair[1] in connections.keys():
+            connections[pair[1]] = []
+        connections[pair[1]].append(pair[0])
+
+    print('Number of paths: ' + str(find_num_paths(connections)))
+
+
+if __name__ == '__main__':
+    main()
